8c9672d9de2be5bc8212f64e2ebe62f0846d2691
antoine <antoinroux@hotmail.fr>
2017-02-19 20:37:46 +0100

    projet sous webpack non fonctionnel au niveau des templates html css de angular2

--- web/js/app/app.component.ts
+++ web/js/app/app.component.ts
@@ -4,20 +4,22 @@ import { Repository } from './repository';
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
-	styles: [`
+    // templateUrl: './app.component.html',
+    // styles: [require('./app.component.css')],
+    template: `<h1>SALUT</h1>`,
+    styles: [`
 		body {
 			font-family: Arial, Helvetica, sans-serif;
 		}
 	`],
-	providers: [RepositoryService]
+    providers: [RepositoryService]
 })
 export class AppComponent implements OnInit {
 	abstract;
 
 	title = 'List des dépôts';
 	repos: Repository[] = [];
+
 	ngOnInit() {
 		this.repoService.getRepo().then(repos => this.repos = repos);
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-module.exports = {  
+module.exports = {
     entry: './web/js/main.ts',
     output: {
         filename: './web/bundles/bundle.js',
         publicPath: "/assets/"
     },
     module: {
-        rules: [{
+        loaders: [{
             test: /\.ts$/,
-            loader: 'ts-loader'
+            loader: ['ts-loader', 'angular2-template-loader?keepUrl=true']
+        }, {
+            test: /\.html$/,
+            loader: 'raw-loader'
+
         }]
     },
     resolve: {
